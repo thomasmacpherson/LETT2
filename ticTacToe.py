a49a5c2581f3073690e5a09c3091cf12d18c078b
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-02-25 17:28:49 +0000

    dff

--- ticTacToe.py
+++ ticTacToe.py
@@ -6,16 +6,23 @@ class thisapp():
 		self.api.setBG()
 
 		self.api.waitForInput(inputReceived)
-		self.pieces = (0,0,0) # player 1, 2 and total pieces
-		self.gridSize = 16
+		self.pieces = (0,0) # player 1, 2
+		self.gridSize = 3
+		
+		self.gridColours = [[2,2,2],
+							[2,2,2],
+							[2,2,2]]
 
 		self.totalGamePlaces = gridSize * gridSize
 		self.turnPrint = ("Opponent's turn", "Your turn")
 		self.winPrint = ("You won", "You lost", "It was a draw")
-		self.playerColours = ((0,0,0),(255,255,255))
+		self.playerColours = ((0,0,255),(255,255,255))
 
 		self.turn = True # player1's turn first
 		self.gridColours
+		
+		self.validPos = (1,2,3,4,6,7,8,9,11,12,13,14)
+		self.placePositions = (1,6,11)
 
 
 	def inputReceived(self, args):
@@ -52,16 +59,24 @@ class thisapp():
 
 	def checkInput(self, args):
 		if args.length == 2:
-		
-			if gridColours[x][y] =! 2:
-				self.api.printLCD(!turn, 1, "Can't go there", 2)	#only displayed for 2 seconds
-				self.api.flashPixel(x, y, Red, 2)
-				return False
+			x = args[0]
+			y = args[1]
 			
+			if validPos.count(x) and validPos.count(y): # is a valid place
+			
+				if gridColours[x][y] =! 2:
+					self.api.printLCD(!turn, 1, "Can't go there", 2)	#only displayed for 2 seconds
+					self.api.flashPixel(x, y, Red, 2)
+					return False
+				
+				else:
+					self.api.setSprite(self.placePositions[], placePostions, playerColours[turn])
+					gridColours[x][y] = turn
+				
+					return True
+					
+					
 			else:
-				self.api.drawPixel(x,y, playerColours[turn])
-				gridColours[x][y] = turn
-			
-				return True
+				self.api.flashPixel(x, y, Red, 2)
 
 
