e8b593bc1cb357334077d70f26f915be84f7d068
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-03-17 19:42:45 +0000

    update

--- lcd.py
+++ lcd.py
@@ -57,11 +57,10 @@ class HD47780(object):
 
 class lcd:
 	def __init__(self, smbus):
-
 		driver1 = LCD_23017(bus=smbus.SMBus(0), addr=0x20, port='B', rs=0, en=1)
    		# driver2 = LCD_23017(bus=smbus.SMBus(1), addr=0x20, port='B', rs=0, en=2)
-
-    	self.lcd1 = HD47780(driver1, rows=2, width=16)
+		
+		self.lcd1 = HD47780(driver1, rows=2, width=16)
    		# lcd2 = HD47780(driver=driver2, rows=2, width=16)
    
    
@@ -76,5 +75,5 @@ class lcd:
 #def main():
 #    test_i2c()
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
