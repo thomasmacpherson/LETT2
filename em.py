b273d5a3699d0cf246c4a739b04c332025699db6
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-03-18 18:58:57 +0000

    update

--- em.py
+++ em.py
@@ -351,14 +351,14 @@ class em:
 	def setPixelColour(self, x, y, colour):
 		if x <16 and y<16:
 			self.pygame.draw.rect(self.screen, colour, (const.gridX+x*20,const.gridY+y*20,20,20),0) 
-			self.pygame.draw.rect(self.screen, (0,0,0), (const.gridX+x*20,const.gridY+y*20,20,20),2) 
+			#self.pygame.draw.rect(self.screen, (255,255,255), (const.gridX+x*20,const.gridY+y*20,20,20),2) 
 
 
 
 	def setPixel(self, x, y): # uses ink colour of corresponding grid
 		if x <16 and y<16:
 			self.pygame.draw.rect(self.screen, self.ink[x/8 + (y/8)*2], (const.gridX+x*20,const.gridY+y*20,20,20),0) 
-			self.pygame.draw.rect(self.screen, (0,0,0), (const.gridX+x*20,const.gridY+y*20,20,20),2)
+			#self.pygame.draw.rect(self.screen, (255,255,255), (const.gridX+x*20,const.gridY+y*20,20,20),2)
 
 
 
