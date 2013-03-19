1f933bd17fb85cdcc6edf811277faac41c26af83
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-03-19 20:48:06 +0000

    update

--- appldr.py
+++ appldr.py
@@ -8,7 +8,7 @@ gameList = ('app','orthello','connect4','draughts','inkspill','pixelArt','simon'
 
 api = api.api(emulated, LEDGrided, LCDed, keyboardHacked)
 
-button = 9 #api.waitForScreenButtonPress()
+button = 11 #api.waitForScreenButtonPress()
 
 #while True:
 	
