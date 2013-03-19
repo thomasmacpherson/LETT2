2c31360e6b089655debf48e01f989b54beac485d
thomasmacpherson <thomasmacpherson@live.co.uk>
2013-03-19 20:16:39 +0000

    update

--- i2cHandler.py
+++ i2cHandler.py
@@ -73,7 +73,7 @@ const.CMD_totalArgs = [
 class handler:
 	def __init__(self, qOut, LEDGrided, LCDed):
 		self.LCDed = LCDed
-		self.LEDGrided
+		self.LEDGrided = LEDGrided
 		self.qOut = qOut
 		self.bus = smbus.SMBus(const.rev2)
 		
